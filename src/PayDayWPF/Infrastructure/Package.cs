b0cdd2876a25c540118f908cd9b9a7ef397feaeb
zer0dayXT <supermegapixel@gmail.com>
2022-05-03 15:57:44 +0200

    Improved grid layout

---
+++ src/PayDayWPF/Infrastructure/Package.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PayDayWPF.Infrastructure
+{
+    internal class Package
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public decimal MeetingProfit { get; set; }
+        public int MeetingCount { get; set; }
+        public List<DateTime> MeetingsHeld { get; set; }
+    }
+}
