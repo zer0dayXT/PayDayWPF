f77b64981e1e8fc83dc3724c735c92d2a53138ff
zer0dayXT <supermegapixel@gmail.com>
2022-05-03 16:38:50 +0200

    Added JsonRepository

---
+++ src/PayDayWPF/Persistence/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using PayDayWPF.Infrastructure;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PayDayWPF.Persistence
+{
+    internal interface IRepository
+    {
+        public Task<List<Package>> Load();
+        public Task AddPackage(Package package);
+    }
+}
---
+++ src/PayDayWPF/Persistence/Implementation/JsonRepository.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using PayDayWPF.Infrastructure;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PayDayWPF.Persistence.Implementation
+{
+    internal class JsonRepository : IRepository
+    {
+        const string Path = @"C:\Users\Zer0day\Documents\PayDayWPF.txt";
+
+        public async Task<List<Package>> Load()
+        {
+            using (var reader = new StreamReader(Path))
+            {
+                return JsonConvert.DeserializeObject<List<Package>>(await reader.ReadLineAsync());
+            }
+        }
+
+        public async Task AddPackage(Package package)
+        {
+            var packages = await Load();
+            packages.Add(package);
+            using (var streamWriter = new StreamWriter(Path))
+            {
+                streamWriter.WriteLine(JsonConvert.SerializeObject(packages));
+            }
+        }
+    }
+}
